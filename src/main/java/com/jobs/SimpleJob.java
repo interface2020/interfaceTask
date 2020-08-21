e22945f69461b3e9d1029d58e32aa0f32d946b48
刘颖 <2573323254@qq.com>
2020-08-21 11:26:20 +0800

    111

--- src/main/java/com/jobs/SimpleJob.java
+++ src/main/java/com/jobs/SimpleJob.java
@@ -29,48 +29,7 @@ public class SimpleJob implements BaseJob {
     }
 
     public void  syncDatas(Pagination page, String url, int times) throws Exception{
-//        warehouseManager.getList(page);
-        JSONObject wareHouseInfo=new JSONObject();
-        wareHouseInfo.put("wareHouseInfo",page.getRows());
-        if(times==0){
-//            int count = warehouseManager.getCount(new HashMap<>());
-//            wareHouseInfo.put("count",count);
-        }
 
-        //数据请求
-        //1.组织请求参数
-        RequestBody body = RequestBody.create(JSONTYPE, wareHouseInfo.toJSONString());
-        Request request = new Request.Builder()
-                .url(url)
-                .post(body)
-                .build();
-        //2.请求
-        Call call = InterfaceConfigHelper.okHttpClient.newCall(request);
-        Response response = call.execute();
-        //3.获取响应报文，如果令牌失败则直接返回
-        String  responseResult = response.body().string();
-        if(response.code() != HttpStatusEnum.OK.getKey()){
-            throw new Exception("同步入库信息至省平台失败【请求HTTP状态为："+ HttpStatusEnum.getValueByKey(response.code())+"("+response.code()+")】", new Exception("网络异常"));
-        }
-        //3.解析响应报文
-        if (responseResult.contains("令牌无效") || responseResult.contains("访问未授权")) {
-            return;
-        }
-        //1.解析结果
-        UploadResponse disUploadResponse = JSON.parseObject(responseResult, new TypeReference<UploadResponse>() {});
-        if (disUploadResponse.getReturnCode().equals("1")){
-            Map uploadResult = new HashMap();
-            uploadResult.put("state", "1");
-            uploadResult.put("list",page.getRows());
-//            warehouseManager.updateUploadResult(uploadResult);
-        }else if (disUploadResponse.getReturnCode().equals("12")){
-            return;
-        }else{
-            throw new Exception("同步入库信息至省平台失败【"+JSONObject.toJSONString(disUploadResponse)+"】", new Exception("处理数据异常"));
-        }
-        if(page.getRecords()>page.getCount()){
-            syncDatas(page,url,1);
-        }
     }
 
 }
